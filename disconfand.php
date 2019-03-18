4955f7b8855e0b05c1e10030a4f5684e8bfe5f30
anonymouseyusuf <yusufprogramer1@gmail.com>
2019-03-18 12:06:23 +0700

    tambahan latihan fungsi if

--- disconfand.php
+++ disconfand.php
@@ -4,33 +4,32 @@
 </head>
 <body>
 <font face=verdana size=2>
-<form action=contoh2_7.php method="post">Jumlah Bayar
+<form action=diskon.php method=post>Jumlah Bayar
 <input type=text name=totalbeli><br><br>
-<input type=submit value="Hitung Diskon">
+<input type=submit value="Hitung Diskon" name=tuku>
 </form>
 
 <?php
 
-$totalbeli=$_POST['totalbeli'];
+
+if(!empty($_POST['tuku'])){
+$totalbeli = $_POST['totalbeli'];
+
 if (isset($totalbeli))
 {
 $toyar=intval($totalbeli);
 $diskon=0;
-if ($toyar>=500000)
-$diskon=(0.5*$toyar);
-else
-if ($toyar>=100000)
-$diskon=(0.1*$toyar);
-else
-if ($toyar>=50000)
-$diskon=(0.05*$toyar);
+if ($toyar>=500000) $diskon=(0.5*$toyar);
+else if ($toyar>=100000) $diskon=(0.1*$toyar);
+else if ($toyar>=50000) $diskon=(0.05*$toyar);
 else
 print("Maaf Tidak Ada Diskon <br>\n");
+printf("Jumlah Bayar = %d<br>\n",$toyar);
 printf("Diskon = %d<br>\n",$diskon);
 $totalbayar=$toyar-$diskon;
 printf("<b>Total Bayar = %d<br></b>\n",$totalbayar);
-printf("Jumlah Bayar = %d<br>\n",$toyar);
-echo number_format($toyar,2,",",".");
+}
+
 }
 ?>
 </body>
